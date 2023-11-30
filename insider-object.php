a14b014bd5d42a2e0b47f565cf183d9cf8ec91ef
egekibar <58031957+egekibar@users.noreply.github.com>
2023-11-30 04:36:49 +0300

    Update insider-object.php

--- insider-object.php
+++ insider-object.php
@@ -14,6 +14,5 @@ use Insider\Insider;
 $object = new Insider();
 
 add_filter( 'site_transient_update_plugins', [$object, 'check_plugin_update'] );
-add_filter( 'transient_update_plugins', [$object, 'check_plugin_update'] );
 
 $object->run();
