5bfd0c7ef8f209a80fd4e0ad885524e8e7e4eb50
egekibar <58031957+egekibar@users.noreply.github.com>
2023-11-30 03:18:59 +0300

    Composer Bug Fixed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bccd01025a6c11f3885114e8fffa3dd3ec54da6e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bccd01025a6c11f3885114e8fffa3dd3ec54da6e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
